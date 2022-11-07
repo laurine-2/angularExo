1c6c9ec0c382a68fa7ddcc221ad407c108220bb4
laurine-2 <laurinembogning2000@gmail.com>
2022-11-07 16:16:01 +0100

    premier exercice

--- projetAngular/src/app/app.component.ts
+++ projetAngular/src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { STUDENTS } from './mock-student-list';
+import { Student } from './Student';
 
 @Component({
   selector: 'app-root',
   template: `
    
       <h1>
-        toto{{title}}!
-      </h1>
+        Liste des etudiants!
+      </h1>  
       
   `,
   styles: []
 })
-export class AppComponent {
-  title:string;
+export class AppComponent implements OnInit{
+  etudiant: Student[] = STUDENTS;
+ ngOnInit() :void{
+  console.table(this.etudiant);
+  console.log(this.etudiant[0].name);
+ }
+
+ selectStudent = (StudentName: string) =>{
+  console.log('vous avez choisir un etudant ${studentName}')
+ }
 }
+
+
+
+
+
+
